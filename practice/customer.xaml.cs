4e9501160f72db86a21a5959bb5126d22df20379
inho-hor <olikaklacc@gmail.com>
2020-12-27 19:59:38 +0300

    тык.

--- practice/customer.xaml.cs
+++ practice/customer.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,61 @@ namespace practice
     /// </summary>
     public partial class customer : Window
     {
+        practiceEntities practiceEntities;
         public customer()
         {
             InitializeComponent();
+            practiceEntities = new practiceEntities();
+            practiceEntities.product.Load();
+            products.ItemsSource = practiceEntities.product.Local.ToBindingList();
+            practiceEntities.cloth.Load();
+            cloths.ItemsSource = practiceEntities.cloth.Local.ToBindingList();
+            practiceEntities.furniture.Load();
+            furnitures.ItemsSource = practiceEntities.furniture.Local.ToBindingList();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void productClick(object sender, RoutedEventArgs e)
+        {
+                products.Visibility = Visibility.Visible;
+                cloths.Visibility = Visibility.Collapsed;
+                furnitures.Visibility = Visibility.Collapsed;
+                var Product = practiceEntities.product;
+                products.ItemsSource = Product.ToList();
+        }
+
+        private void clothClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void furnitureClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ordersClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void constructorClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void orderClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void exitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
