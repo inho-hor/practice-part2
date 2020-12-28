e00a13c10c4d19ece640f4057ce839315c704d04
inho-hor <olikaklacc@gmail.com>
2020-12-28 09:08:53 +0300

    менеджер.

--- practice/manager.xaml.cs
+++ practice/manager.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,115 @@ namespace practice
     /// </summary>
     public partial class manager : Window
     {
+        practiceEntities practiceEntities;
+
         public manager()
         {
             InitializeComponent();
+            practiceEntities = new practiceEntities();
+            practiceEntities.product.Load();
+            products.ItemsSource = practiceEntities.product.Local.ToBindingList();
+            practiceEntities.cloth.Load();
+            cloths.ItemsSource = practiceEntities.cloth.Local.ToBindingList();
+            practiceEntities.furniture.Load();
+            furnitures.ItemsSource = practiceEntities.furniture.Local.ToBindingList();
+            practiceEntities.clothWarehouse.Load();
+            clothWare.ItemsSource = practiceEntities.clothWarehouse.Local.ToBindingList();
+            practiceEntities.furnitureWarehouse.Load();
+            furnitureWare.ItemsSource = practiceEntities.furnitureWarehouse.Local.ToBindingList();
+        }
+
+        private void exitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void productClick(object sender, RoutedEventArgs e)
+        {
+            products.Visibility = Visibility.Visible;
+            cloths.Visibility = Visibility.Collapsed;
+            furnitures.Visibility = Visibility.Collapsed;
+            orders.Visibility = Visibility.Collapsed;
+            furnitureWare.Visibility = Visibility.Collapsed;
+            clothWare.Visibility = Visibility.Collapsed;
+
+            var Product = practiceEntities.product;
+            products.ItemsSource = Product.ToList();
+        }
+
+        private void clothClick(object sender, RoutedEventArgs e)
+        {
+            products.Visibility = Visibility.Collapsed;
+            cloths.Visibility = Visibility.Visible;
+            furnitures.Visibility = Visibility.Collapsed;
+            orders.Visibility = Visibility.Collapsed;
+            furnitureWare.Visibility = Visibility.Collapsed;
+            clothWare.Visibility = Visibility.Collapsed;
+
+            var Cloth = practiceEntities.cloth;
+            cloths.ItemsSource = Cloth.ToList();
+        }
+
+        private void furnitureClick(object sender, RoutedEventArgs e)
+        {
+            products.Visibility = Visibility.Collapsed;
+            cloths.Visibility = Visibility.Collapsed;
+            furnitures.Visibility = Visibility.Visible;
+            orders.Visibility = Visibility.Collapsed;
+            furnitureWare.Visibility = Visibility.Collapsed;
+            clothWare.Visibility = Visibility.Collapsed;
+
+            var Furniture = practiceEntities.furniture;
+            furnitures.ItemsSource = Furniture.ToList();
+        }
+
+        private void wareClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clothWareClick(object sender, RoutedEventArgs e)
+        {
+            products.Visibility = Visibility.Collapsed;
+            cloths.Visibility = Visibility.Collapsed;
+            furnitures.Visibility = Visibility.Collapsed;
+            orders.Visibility = Visibility.Collapsed;
+            clothWare.Visibility = Visibility.Visible;
+            furnitureWare.Visibility = Visibility.Collapsed;
+
+            var clothWareHouse = practiceEntities.clothWarehouse;
+            clothWare.ItemsSource = clothWareHouse.ToList();
+        }
+
+        private void furnitureWareClick(object sender, RoutedEventArgs e)
+        {
+            products.Visibility = Visibility.Collapsed;
+            cloths.Visibility = Visibility.Collapsed;
+            furnitures.Visibility = Visibility.Collapsed;
+            orders.Visibility = Visibility.Collapsed;
+            furnitureWare.Visibility = Visibility.Visible;
+            clothWare.Visibility = Visibility.Collapsed;
+
+            var furnitureWareHouse = practiceEntities.furnitureWarehouse;
+            furnitureWare.ItemsSource = furnitureWareHouse.ToList();
+        }
+
+        private void orderClick(object sender, RoutedEventArgs e)
+        {
+            products.Visibility = Visibility.Collapsed;
+            cloths.Visibility = Visibility.Collapsed;
+            furnitures.Visibility = Visibility.Collapsed;
+            orders.Visibility = Visibility.Visible;
+            furnitureWare.Visibility = Visibility.Collapsed;
+            clothWare.Visibility = Visibility.Collapsed;
+
+            var Order = practiceEntities.order;
+            orders.ItemsSource = Order.ToList();
         }
     }
 }
